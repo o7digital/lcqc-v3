889e22e353643beda32c2b1cbf852cf92e697561
o7digital <101292703+o7digital@users.noreply.github.com>
2025-10-10 20:16:48 -0600

    Use home 12 as default landing page

---
+++ cusi/app/page.jsx
@@ -0,0 +1,13 @@
+import HomePage12 from "./(homes)/home-12/page";
+
+export const metadata = {
+  title: "Home 12 || Uomo eCommerce React Nextjs Template",
+  description: "Uomo eCommerce React Nextjs Template",
+};
+export default function Home() {
+  return (
+    <>
+      <HomePage12 />
+    </>
+  );
+}
