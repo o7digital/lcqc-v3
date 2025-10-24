ea9c0b0664e9ac8b39ac388321d1fee183bce1d0
o7digital <101292703+o7digital@users.noreply.github.com>
2025-10-23 21:41:48 -0600

    Nettoyage: suppression des dossiers indésirables

--- touski/utlis/openCart.js
+++
@@ -1,6 +0,0 @@
-export const openCart = () => {
-  document
-    .getElementById("cartDrawerOverlay")
-    .classList.add("page-overlay_visible");
-  document.getElementById("cartDrawer").classList.add("aside_visible");
-};
